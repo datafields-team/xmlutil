fa0fb1994caf7db7d54b4b52738000edc4feb143
yangaound <yangaound@users.noreply.github.com>
2017-10-11 01:32:13 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+version = '1.0.0'
+
+package_dir = 'xmlutil'
+
+setup(name='xmlutil',
+
+    version=version,
+
+    description='a small tool for extract multiple xml file to a table.',
+
+    packages=[package_dir, ],
+)
